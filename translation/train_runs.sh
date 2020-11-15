08585216f750b9b9070723d9ba18d06721815ad4
Sea-Snell <sea_snell@icloud.com>
2020-11-15 15:03:45 -0800

    changed epochs

--- translation/train_runs.sh
+++ translation/train_runs.sh
@@ -4,17 +4,17 @@
 # python train_translation.py --epochs 20 --dataset multi30k --seed 13 --save_every 2000 --config configs/model.json
 # python train_translation.py --epochs 20 --dataset multi30k --seed 13 --save_every 2000 --uniform --config configs/model.json
 
-# python train_translation.py --epochs 20 --dataset iwslt14 --seed 20 --save_every 2000 --config configs/model.json
-# python train_translation.py --epochs 20 --dataset iwslt14 --seed 13 --save_every 2000 --config configs/model.json
-# python train_translation.py --epochs 20 --dataset iwslt14 --seed 13 --save_every 2000 --uniform --config configs/model.json
+# python train_translation.py --epochs 30 --dataset iwslt14 --seed 20 --save_every 2000 --config configs/model.json
+# python train_translation.py --epochs 30 --dataset iwslt14 --seed 13 --save_every 2000 --config configs/model.json
+# python train_translation.py --epochs 30 --dataset iwslt14 --seed 13 --save_every 2000 --uniform --config configs/model.json
 
 python train_translation.py --epochs 20 --dataset news_commentary_v14_en_nl --seed 20 --save_every 2000 --config configs/model.json
 python train_translation.py --epochs 20 --dataset news_commentary_v14_en_nl --seed 13 --save_every 2000 --config configs/model.json
 python train_translation.py --epochs 20 --dataset news_commentary_v14_en_nl --seed 13 --save_every 2000 --uniform --config configs/model.json
 
-# python train_translation.py --epochs 20 --dataset news_commentary_v14_cs_fr --seed 20 --save_every 2000 --config configs/model.json
-# python train_translation.py --epochs 20 --dataset news_commentary_v14_cs_fr --seed 13 --save_every 2000 --config configs/model.json
-# python train_translation.py --epochs 20 --dataset news_commentary_v14_cs_fr --seed 13 --save_every 2000 --uniform --config configs/model.json
+# python train_translation.py --epochs 30 --dataset news_commentary_v14_cs_fr --seed 20 --save_every 2000 --config configs/model.json
+# python train_translation.py --epochs 30 --dataset news_commentary_v14_cs_fr --seed 13 --save_every 2000 --config configs/model.json
+# python train_translation.py --epochs 30 --dataset news_commentary_v14_cs_fr --seed 13 --save_every 2000 --uniform --config configs/model.json
 
 python train_translation.py --epochs 20 --dataset news_commentary_v14_ar_id --seed 20 --save_every 2000 --config configs/model.json
 python train_translation.py --epochs 20 --dataset news_commentary_v14_ar_id --seed 13 --save_every 2000 --config configs/model.json
