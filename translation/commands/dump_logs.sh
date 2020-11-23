5ed57f43ff73655a0cb48201c030bcfcaac7b328
Sea-Snell <sea_snell@icloud.com>
2020-11-22 21:57:57 -0600

    fixed command path

--- translation/commands/dump_logs.sh
+++ translation/commands/dump_logs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python eval_dump.py --dataset multi30k --eval_bleu --include_train_subset --grad_bsize 32
-python eval_dump.py --dataset iwslt14 --eval_bleu --include_train_subset --grad_bsize 32
-python eval_dump.py --dataset news_commentary_v14_en_nl --eval_bleu --include_train_subset --grad_bsize 32
-python eval_dump.py --dataset news_commentary_v14_en_pt --eval_bleu --include_train_subset --grad_bsize 32
-python eval_dump.py --dataset news_commentary_v14_it_pt --eval_bleu --include_train_subset --grad_bsize 32
+python ../eval_dump.py --dataset multi30k --eval_bleu --include_train_subset --grad_bsize 32
+python ../eval_dump.py --dataset iwslt14 --eval_bleu --include_train_subset --grad_bsize 32
+python ../eval_dump.py --dataset news_commentary_v14_en_nl --eval_bleu --include_train_subset --grad_bsize 32
+python ../eval_dump.py --dataset news_commentary_v14_en_pt --eval_bleu --include_train_subset --grad_bsize 32
+python ../eval_dump.py --dataset news_commentary_v14_it_pt --eval_bleu --include_train_subset --grad_bsize 32
--- translation/commands/train_runs.sh
+++ translation/commands/train_runs.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-python train_translation.py --epochs 20 --dataset multi30k --seed 20 --save_every 2000 --config configs/model.json --custom_saves 0,50,100,500,1000,1500
-python train_translation.py --epochs 20 --dataset multi30k --seed 13 --save_every 2000 --config configs/model.json --custom_saves 0,50,100,500,1000,1500
-python train_translation.py --epochs 20 --dataset multi30k --seed 12 --save_every 2000 --uniform --config configs/model.json --custom_saves 0,50,100,500,1000,1500
+python ../train_translation.py --epochs 20 --dataset multi30k --seed 20 --save_every 2000 --config configs/model.json --custom_saves 0,50,100,500,1000,1500
+python ../train_translation.py --epochs 20 --dataset multi30k --seed 13 --save_every 2000 --config configs/model.json --custom_saves 0,50,100,500,1000,1500
+python ../train_translation.py --epochs 20 --dataset multi30k --seed 12 --save_every 2000 --uniform --config configs/model.json --custom_saves 0,50,100,500,1000,1500
 
-python train_translation.py --epochs 30 --dataset iwslt14 --seed 20 --save_every 2000 --config configs/model.json --custom_saves 0,50,100,500,1000,1500
-python train_translation.py --epochs 30 --dataset iwslt14 --seed 13 --save_every 2000 --config configs/model.json --custom_saves 0,50,100,500,1000,1500
-python train_translation.py --epochs 30 --dataset iwslt14 --seed 12 --save_every 2000 --uniform --config configs/model.json --custom_saves 0,50,100,500,1000,1500
+python ../train_translation.py --epochs 30 --dataset iwslt14 --seed 20 --save_every 2000 --config configs/model.json --custom_saves 0,50,100,500,1000,1500
+python ../train_translation.py --epochs 30 --dataset iwslt14 --seed 13 --save_every 2000 --config configs/model.json --custom_saves 0,50,100,500,1000,1500
+python ../train_translation.py --epochs 30 --dataset iwslt14 --seed 12 --save_every 2000 --uniform --config configs/model.json --custom_saves 0,50,100,500,1000,1500
 
-python train_translation.py --epochs 40 --dataset news_commentary_v14_en_nl --seed 20 --save_every 2000 --config configs/model.json --custom_saves 0,50,100,500,1000,1500
-python train_translation.py --epochs 40 --dataset news_commentary_v14_en_nl --seed 13 --save_every 2000 --config configs/model.json --custom_saves 0,50,100,500,1000,1500
-python train_translation.py --epochs 40 --dataset news_commentary_v14_en_nl --seed 12 --save_every 2000 --uniform --config configs/model.json --custom_saves 0,50,100,500,1000,1500
+python ../train_translation.py --epochs 40 --dataset news_commentary_v14_en_nl --seed 20 --save_every 2000 --config configs/model.json --custom_saves 0,50,100,500,1000,1500
+python ../train_translation.py --epochs 40 --dataset news_commentary_v14_en_nl --seed 13 --save_every 2000 --config configs/model.json --custom_saves 0,50,100,500,1000,1500
+python ../train_translation.py --epochs 40 --dataset news_commentary_v14_en_nl --seed 12 --save_every 2000 --uniform --config configs/model.json --custom_saves 0,50,100,500,1000,1500
 
-python train_translation.py --epochs 30 --dataset news_commentary_v14_en_pt --seed 20 --save_every 2000 --config configs/model.json --custom_saves 0,50,100,500,1000,1500
-python train_translation.py --epochs 30 --dataset news_commentary_v14_en_pt --seed 13 --save_every 2000 --config configs/model.json --custom_saves 0,50,100,500,1000,1500
-python train_translation.py --epochs 30 --dataset news_commentary_v14_en_pt --seed 12 --save_every 2000 --uniform --config configs/model.json --custom_saves 0,50,100,500,1000,1500
+python ../train_translation.py --epochs 30 --dataset news_commentary_v14_en_pt --seed 20 --save_every 2000 --config configs/model.json --custom_saves 0,50,100,500,1000,1500
+python ../train_translation.py --epochs 30 --dataset news_commentary_v14_en_pt --seed 13 --save_every 2000 --config configs/model.json --custom_saves 0,50,100,500,1000,1500
+python ../train_translation.py --epochs 30 --dataset news_commentary_v14_en_pt --seed 12 --save_every 2000 --uniform --config configs/model.json --custom_saves 0,50,100,500,1000,1500
 
-python train_translation.py --epochs 40 --dataset news_commentary_v14_it_pt --seed 20 --save_every 2000 --config configs/model.json --custom_saves 0,50,100,500,1000,1500
-python train_translation.py --epochs 40 --dataset news_commentary_v14_it_pt --seed 13 --save_every 2000 --config configs/model.json --custom_saves 0,50,100,500,1000,1500
-python train_translation.py --epochs 40 --dataset news_commentary_v14_it_pt --seed 12 --save_every 2000 --uniform --config configs/model.json --custom_saves 0,50,100,500,1000,1500
+python ../train_translation.py --epochs 40 --dataset news_commentary_v14_it_pt --seed 20 --save_every 2000 --config configs/model.json --custom_saves 0,50,100,500,1000,1500
+python ../train_translation.py --epochs 40 --dataset news_commentary_v14_it_pt --seed 13 --save_every 2000 --config configs/model.json --custom_saves 0,50,100,500,1000,1500
+python ../train_translation.py --epochs 40 --dataset news_commentary_v14_it_pt --seed 12 --save_every 2000 --uniform --config configs/model.json --custom_saves 0,50,100,500,1000,1500
